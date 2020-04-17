629a0abc98aaf3d1810ac03adf59ce3492ddbde9
thavananthan <it17068192@my.sliit.lk>
2020-04-18 00:36:37 +0530

    build

---
+++ build/precache-manifest.82a2fb4624aeb5f498d796b748f1d8e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ec4b7cd6e4d91f440b0533becd1857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56720e0c98edf537cf51",
+    "url": "/static/css/main.bef2ea31.chunk.css"
+  },
+  {
+    "revision": "b891c3708340412bef33",
+    "url": "/static/js/2.746cdc7a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.746cdc7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56720e0c98edf537cf51",
+    "url": "/static/js/main.dd89c134.chunk.js"
+  },
+  {
+    "revision": "6909ddb41416de7848ac",
+    "url": "/static/js/runtime-main.dac0035b.js"
+  }
+]);
